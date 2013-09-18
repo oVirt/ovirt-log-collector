c75e1f2a926fb8fdd4229dae150ac8546ef1f8fb
Sandro Bonazzola <sbonazzo@redhat.com>
2013-09-18 13:52:30 +0200

    sos: engine: add jboss stack trace
    added jbosstrace option, enabled by default, to engine
    sos plugin.
    The new option send a SIGQUIT signal to the JBoss
    process launched by ovirt-engine, causing a dump of
    the stack trace to /var/log/ovirt-engine/console.log.
    
    Other JBoss instances will not receive that signal.
    
    Change-Id: Id049f1cc7f322995a8692562a3f7aebddc7cc04d
    Bug-Url: https://bugzilla.redhat.com/989484
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- src/sos/plugins/engine.py
+++ src/sos/plugins/engine.py
@@ -1,3 +1,8 @@
+import os
+import signal
+import subprocess
+
+
 import sos.plugintools
 
 
@@ -5,7 +10,59 @@ import sos.plugintools
 class engine(sos.plugintools.PluginBase):
     """oVirt related information"""
 
+    optionList = [
+        (
+            'jbosstrace',
+            'Enable oVirt Engine JBoss stack trace generation',
+            '',
+            True
+        ),
+    ]
+
+    def __init__(self, pluginname, commons):
+        sos.plugintools.PluginBase.__init__(self, pluginname, commons)
+
     def setup(self):
+        if self.getOption('jbosstrace'):
+            proc = subprocess.Popen(
+                args=[
+                    '/usr/bin/pgrep',
+                    '-f',
+                    'jboss',
+                ],
+                stdout=subprocess.PIPE,
+            )
+            output, err = proc.communicate()
+            returncode = proc.returncode
+            jboss_pids = set()
+            if returncode == 0:
+                jboss_pids = set([int(x) for x in output.splitlines()])
+                proc = subprocess.Popen(
+                    args=[
+                        '/usr/bin/pgrep',
+                        '-f',
+                        'ovirt-engine',
+                    ],
+                    stdout=subprocess.PIPE,
+                )
+                engine_output, err = proc.communicate()
+                if returncode == 0:
+                    engine_pids = set(
+                        [int(x) for x in engine_output.splitlines()]
+                    )
+                    jboss_pids.intersection_update(engine_pids)
+                else:
+                    self.soslog.error('Unable to get engine pids: %s' % err)
+                    self.addAlert('Unable to get engine pids')
+            else:
+                self.soslog.error('Unable to get jboss pid: %s' % err)
+                self.addAlert('Unable to get jboss pid')
+            for pid in jboss_pids:
+                try:
+                    os.kill(pid, signal.SIGQUIT)
+                except OSError as e:
+                    self.soslog.error('Unable to send signal to %d' % pid, e)
+
         # Copy engine config files.
         self.addCopySpec("/etc/ovirt-engine")
         self.addCopySpec("/etc/rhevm")
