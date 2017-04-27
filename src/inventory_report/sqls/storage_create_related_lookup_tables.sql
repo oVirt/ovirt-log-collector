5abca0e54703a3f616438bb18ce3f656662b1726
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-04-27 16:22:49 -0400

    inventory_report: Add documentation about tables values
    Code readability improvement.
    
    Change-Id: Ia35ea91f812e9b9a2a564770c054abef2babc7bc
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/sqls/storage_create_related_lookup_tables.sql
+++ src/inventory_report/sqls/storage_create_related_lookup_tables.sql
@@ -2,6 +2,10 @@ CREATE table storage_type_temp (
     id NUMERIC, text varchar
 );
 
+----------------------------------------------------------------
+-- All values defined in ovirt-engine project:
+-- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/storage/StorageType.java
+----------------------------------------------------------------
 INSERT INTO storage_type_temp VALUES
     (0, 'UNKNOWN'),
     (1, 'NFS'),
@@ -17,6 +21,10 @@ CREATE table storage_domain_type_temp (
     id NUMERIC, text varchar
 );
 
+----------------------------------------------------------------
+-- All values defined in ovirt-engine project:
+-- backend/manager/modules/common/src/main/java/org/ovirt/engine/core/common/businessentities/StorageDomainType.java
+----------------------------------------------------------------
 INSERT into storage_domain_type_temp VALUES
     (0, 'Master'),
     (1, 'Data'),
