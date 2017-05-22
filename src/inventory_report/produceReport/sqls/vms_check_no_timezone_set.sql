b7027a875f47c02adf6653b49cc5097b5008d2bf
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-05-22 14:53:30 -0400

    inventory: check if vms timezone are not set
    Share in the report vms without timezone data
    
    Change-Id: I7a75096e4d89f5df2465ae8e7e38fc6b3f92e62f
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

---
+++ src/inventory_report/produceReport/sqls/vms_check_no_timezone_set.sql
@@ -0,0 +1,14 @@
+SELECT
+    vm_name AS "VM Name",
+    os AS "OS",
+    time_zone as "TimeZone"
+FROM
+    vm_static
+WHERE
+    time_zone = '' AND
+    vm_name != 'Tiny' AND
+    vm_name != 'XLarge' AND
+    vm_name != 'Small' AND
+    vm_name != 'Large' AND
+    vm_name != 'Medium' AND
+    vm_name != 'Blank'
