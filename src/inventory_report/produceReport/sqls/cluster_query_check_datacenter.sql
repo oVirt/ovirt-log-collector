5b70462911fc900f07645af9c1a177235fd81704
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-06-09 15:01:47 -0400

    make cluster_query_check_datacenter.sql compat with db 4.x
    Previously, the query only works with vds_groups table.
    
    Change-Id: I48fd281871da4ea13a43461833acd6d0b603eb28
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/cluster_query_check_datacenter.sql
+++ src/inventory_report/produceReport/sqls/cluster_query_check_datacenter.sql
@@ -1,6 +1,32 @@
-SELECT
-    name
-FROM
-    vds_groups
-WHERE
-    storage_pool_id IS NULL
+CREATE OR REPLACE FUNCTION __temp_cluster_check_datacenter_assigned()
+  RETURNS TABLE(name VARCHAR(40)) AS
+$PROCEDURE$
+BEGIN
+    -- In the Engine db 4.0, vds_groups has been renamed
+    -- to cluster.
+    IF EXISTS (SELECT column_name
+               FROM information_schema.columns
+               WHERE table_name='cluster') THEN
+        RETURN QUERY EXECUTE format('
+        SELECT
+            name
+        FROM
+            cluster
+        WHERE
+            storage_pool_id IS NULL
+        ');
+    ELSE
+        -- Compat mode, engine database < 4.0
+        RETURN QUERY EXECUTE format('
+        SELECT
+            name
+        FROM
+            vds_groups
+        WHERE
+            storage_pool_id IS NULL
+        ');
+    END IF;
+END; $PROCEDURE$
+LANGUAGE plpgsql;
+SELECT __temp_cluster_check_datacenter_assigned();
+DROP FUNCTION __temp_cluster_check_datacenter_assigned();
