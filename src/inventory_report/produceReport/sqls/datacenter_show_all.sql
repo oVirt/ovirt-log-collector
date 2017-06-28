6740f4066a6c4c4494e25471c5382516c24d7006
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-06-28 15:33:56 -0400

    inventory: Display compatibility in DC Section
    This patch adds a table to DC section and the compatibility version
    data.
    
    Change-Id: I7570f6e432fd059a6833071164d56c82a9b4346c
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/datacenter_show_all.sql
+++ src/inventory_report/produceReport/sqls/datacenter_show_all.sql
@@ -1,5 +1,7 @@
 SELECT
-    name AS "Data Center"
+    row_number() OVER (ORDER BY name NULLs last) AS "NO.",
+    name AS "Data Center",
+    compatibility_version AS "Compatibility version"
 FROM
     storage_pool
 ORDER BY name
