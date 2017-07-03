14b354cfba69325422dfb85126ecce7aeee33c7e
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-07-03 11:07:22 -0400

    inventory: storage domains - Add used/available/total disks size
    Change-Id: I7c3f4318728533e935d6df841751b9c055b84125
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/produceReport.sh
+++ src/inventory_report/produceReport/produceReport.sh
@@ -332,10 +332,14 @@ QUERY_STORAGE_DOMAIN="SELECT
       $(projectionCountingRowsWithOrder sds.storage_name),
       sds.storage_name AS \"Storage Domain\",
       stt.text AS \"Type\",
-      sdtt.text AS \"Storage Domain Type\"
+      sdtt.text AS \"Storage Domain Type\",
+      sds.available_disk_size AS \"Available disk size (GB)\",
+      sds.used_disk_size AS \"Used disk size (GB)\",
+      sum(sds.available_disk_size + sds.used_disk_size) AS \"Total disk size (GB)\"
     FROM storage_domains sds
     JOIN storage_type_temp stt ON sds.storage_type=stt.id
     JOIN storage_domain_type_temp sdtt ON sds.storage_domain_type=sdtt.id
+    GROUP BY sds.storage_name, sds.available_disk_size, sds.used_disk_size, stt.text, sdtt.text
     ORDER BY sds.storage_name"
 
 QUERY_STORAGE_DOMAIN_AS_CSV=$(createStatementExportingToCsvFromSelect "$QUERY_STORAGE_DOMAIN" )
