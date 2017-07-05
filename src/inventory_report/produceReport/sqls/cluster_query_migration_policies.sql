ca05a1a5c5005655f4f4a495d492fd53450f6082
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-07-05 11:16:15 -0400

    inventory: Add Cluster Migration Policies table
    Change-Id: I5692811b6a84da800279b0779f75db8a521f4b9d
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

---
+++ src/inventory_report/produceReport/sqls/cluster_query_migration_policies.sql
@@ -0,0 +1,46 @@
+CREATE OR REPLACE FUNCTION __temp_cluster_migration_policies()
+  RETURNS TABLE(name VARCHAR(40), policyname VARCHAR(128), properties text) AS
+$PROCEDURE$
+BEGIN
+    -- In the Engine db 4.0, vds_groups has been renamed
+    -- to cluster.
+    IF EXISTS (SELECT column_name
+               FROM information_schema.columns
+               WHERE table_name='cluster') THEN
+        RETURN QUERY (
+        SELECT
+            cluster.name AS "Cluster Name",
+            cluster_policies.name AS "Policy Name",
+            cluster_policies.custom_properties AS "Properties"
+        FROM
+            cluster
+        INNER JOIN cluster_policies ON cluster.cluster_policy_id=cluster_policies.id
+        );
+    ELSE
+        -- Compat mode, engine database < 4.0
+        RETURN QUERY (
+        SELECT
+            vds_groups.name AS "Cluster Name",
+            cluster_policies.name AS "Policy Name",
+            cluster_policies.custom_properties AS "Properties"
+        FROM
+            vds_groups
+        INNER JOIN cluster_policies ON vds_groups.cluster_policy_id=cluster_policies.id
+        );
+    END IF;
+END; $PROCEDURE$
+LANGUAGE plpgsql;
+
+COPY (
+  SELECT
+    row_number() OVER (ORDER BY name NULLs last) AS "NO.",
+    name AS "Cluster",
+    policyname AS "Policy Name",
+    properties AS "Properties"
+  FROM
+    __temp_cluster_migration_policies()
+  ORDER BY
+    name
+) To STDOUT With CSV DELIMITER E'\|' HEADER;
+
+DROP FUNCTION __temp_cluster_migration_policies();
