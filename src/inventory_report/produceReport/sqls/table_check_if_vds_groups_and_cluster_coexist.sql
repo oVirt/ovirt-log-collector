49ac841c8e348cbe42af0d1f6b1862a0aae032fa
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-07-05 11:16:42 -0400

    inventory: remove check_if_vds_groups_and_cluster_coexist
    vds_groups is a view and will co-exist with cluster.
    It's related to an upgrade issue in commit
    b3740cdeeb6d12fc6aeeff9f2d3f1e1adaee71c1
    
    Change-Id: I11203a780076c22037bc8ecbed7e65073bfc36e7
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/table_check_if_vds_groups_and_cluster_coexist.sql
+++
@@ -1,7 +0,0 @@
--- vds_groups table has been renamed to cluster and should not co-exist
-SELECT
-    COUNT(1)
-FROM
-    information_schema.tables
-WHERE
-    table_name IN ('cluster', 'vds_groups');
