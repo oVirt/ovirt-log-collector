ca888691fdf7f9f7b3b5f7b3a863d7042a266767
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-07-10 12:03:07 -0400

    inventory: Add DC into storage domain table
    Change-Id: If96a2c6fc5c26fb9cad82980fc0937854248604a
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/produceReport.sh
+++ src/inventory_report/produceReport/produceReport.sh
@@ -335,6 +335,7 @@ printSection "Storage Domains"
 QUERY_STORAGE_DOMAIN="SELECT
       $(projectionCountingRowsWithOrder sds.storage_name),
       sds.storage_name AS \"Storage Domain\",
+      sds.storage_pool_name AS \"Data Center\",
       stt.text AS \"Type\",
       sdtt.text AS \"Storage Domain Type\",
       sds.available_disk_size AS \"Available disk size (GB)\",
@@ -343,7 +344,7 @@ QUERY_STORAGE_DOMAIN="SELECT
     FROM storage_domains sds
     JOIN storage_type_temp stt ON sds.storage_type=stt.id
     JOIN storage_domain_type_temp sdtt ON sds.storage_domain_type=sdtt.id
-    GROUP BY sds.storage_name, sds.available_disk_size, sds.used_disk_size, stt.text, sdtt.text
+    GROUP BY sds.storage_name, sds.storage_pool_name, sds.available_disk_size, sds.used_disk_size, stt.text, sdtt.text
     ORDER BY sds.storage_name"
 
 QUERY_STORAGE_DOMAIN_AS_CSV=$(createStatementExportingToCsvFromSelect "$QUERY_STORAGE_DOMAIN" )
