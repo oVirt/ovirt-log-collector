7ea249f56d0219c6599ed4bc3c81a6b746c2d76c
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-08-04 11:22:07 -0400

    inventory: merge storage domain and disks section
    Change-Id: I2fb829ed86fdf2b36ef1c8e073c4125f90c5df14
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/storage_domains_query_data.sql
+++ src/inventory_report/produceReport/sqls/storage_domains_query_data.sql
@@ -7,11 +7,13 @@ COPY (
         sdtt.text AS "Storage Domain Type",
         sds.available_disk_size AS "Available disk size (GB)",
         sds.used_disk_size AS "Used disk size (GB)",
-        sum(sds.available_disk_size + sds.used_disk_size) AS "Total disk size (GB)"
+        sum(sds.available_disk_size + sds.used_disk_size) AS "Total disk size (GB)",
+        count(image_storage_domain_map.image_id) AS "Number of disks"
     FROM
         storage_domains sds
     JOIN storage_type_temp stt ON sds.storage_type=stt.id
     JOIN storage_domain_type_temp sdtt ON sds.storage_domain_type=sdtt.id
+    JOIN image_storage_domain_map ON sds.id=image_storage_domain_map.storage_domain_id
     GROUP BY
         sds.storage_name, sds.storage_pool_name, sds.available_disk_size, sds.used_disk_size, stt.text, sdtt.text
     ORDER BY
