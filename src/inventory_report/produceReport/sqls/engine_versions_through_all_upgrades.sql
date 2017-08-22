597314ccfb09787de3793ff911041d552bf412da
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-08-22 13:03:28 -0400

    inventory: Add copyright header for the SQLs files
    Change-Id: I088901bc4a27ed896e562e115c1570fc4351a1c0
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/engine_versions_through_all_upgrades.sql
+++ src/inventory_report/produceReport/sqls/engine_versions_through_all_upgrades.sql
@@ -1,3 +1,16 @@
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--
+--
 -- This script takes all records from schema_version, and groups them 'according' to time, when their execution ended.
 -- But grouping would create I record per group, which is not desirable. Therefore we 'round' their execution end time
 -- to 30 minutes intervals, and group records using this time. From each such group we extract version column having
