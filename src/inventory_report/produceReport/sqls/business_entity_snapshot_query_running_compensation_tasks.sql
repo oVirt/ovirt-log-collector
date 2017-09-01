75bf258b39c5f861ba43b884741728c834063753
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-09-01 09:57:11 -0400

    inventory: Improve table display to compensation tasks
    Change-Id: I0020cf0b6f70fe776761e85ee3edbc1bc6f5d22b
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/business_entity_snapshot_query_running_compensation_tasks.sql
+++ src/inventory_report/produceReport/sqls/business_entity_snapshot_query_running_compensation_tasks.sql
@@ -13,20 +13,29 @@
 -- Based on https://github.com/oVirt/ovirt-engine/blob/0d39c2580caf4597cbdbaaa5de0282b24089cb68/packaging/setup/plugins/ovirt-engine-setup/ovirt-engine/upgrade/asynctasks.py#L262
 --
 CREATE OR REPLACE FUNCTION __temp_query_get_compensation_tasks()
-  RETURNS TABLE(command_type VARCHAR(256), entity_type VARCHAR(128)) AS
+  RETURNS TABLE(cmd_type VARCHAR(256), ent_type VARCHAR(128)) AS
 $PROCEDURE$
 BEGIN
     IF EXISTS (SELECT column_name
                FROM information_schema.columns
                WHERE table_name='business_entity_snapshot') THEN
-        RETURN QUERY EXECUTE format('
+        RETURN QUERY (
         SELECT
-            command_type, entity_type
+            command_type,
+            entity_type
         FROM
             business_entity_snapshot
-       ');
+        );
    END IF;
 END; $PROCEDURE$
 LANGUAGE plpgsql;
-SELECT __temp_query_get_compensation_tasks();
+
+COPY (
+    SELECT
+        cmd_type AS "Command Type",
+        ent_type AS "Entity Type"
+    FROM
+        __temp_query_get_compensation_tasks()
+) TO STDOUT WITH CSV DELIMITER E'\|' HEADER;
+
 DROP FUNCTION __temp_query_get_compensation_tasks();
