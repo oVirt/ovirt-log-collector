30764c758bf9e91e4516dae82b925f9557fc238b
Douglas Schilling Landgraf <dougsland@redhat.com>
2017-09-01 09:51:29 -0400

    inventory: Add copyright header for the SQLs files
    Change-Id: I088901bc4a27ed896e562e115c1570fc4351a1c0
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/produceReport/sqls/mac_pools_query_show_data_based_on_datacenter_and_cluster.sql
+++ src/inventory_report/produceReport/sqls/mac_pools_query_show_data_based_on_datacenter_and_cluster.sql
@@ -1,3 +1,15 @@
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+--
 CREATE OR REPLACE FUNCTION __temp_mac_pools()
   RETURNS TABLE(cluster_name VARCHAR(40),mac_name VARCHAR(255), mac_desc VARCHAR(4000), mac_dup boolean, mac_default_pool boolean, range text) AS
 $PROCEDURE$
