313444031be34d3627e9529f95f3f1cabfab6fcd
Sandro Bonazzola <sbonazzo@redhat.com>
2018-02-20 13:39:18 +0100

    build: post 4.2.4
    Change-Id: I234a898815caf9c587fcea62cf057cdc4325f2e6
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -5,6 +5,7 @@
 [[ -d tmp.repos ]] \
 || mkdir -p tmp.repos
 
+SUFFIX=".$(date -u +%Y%m%d%H%M%S).git$(git rev-parse --short HEAD)"
 
 autoreconf -ivf
 ./configure
@@ -12,6 +13,7 @@ make dist
 yum-builddep ovirt-log-collector.spec
 rpmbuild \
     -D "_topdir $PWD/tmp.repos" \
+    -D "release_suffix ${SUFFIX}" \
     -ta ovirt-log-collector-*.tar.gz
 
 mv *.tar.gz exported-artifacts
