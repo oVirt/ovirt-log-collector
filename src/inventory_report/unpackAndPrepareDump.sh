a296f061239c42bc172c45ba6b5fe494104de289
Douglas Schilling Landgraf <dougsland@redhat.com>
2018-02-20 22:07:02 -0500

    inventory: handle to postgresql dumps from sosreport
    Recent sosreports can bring two sql dumps, pgdump-scl-rh-postgresql95.tar
    and pgdump.tar. In the past, we only look for one sql dump in the sosreport,
    this patch adds a try/compat mode test.
    
    ref: https://github.com/oVirt/ovirt-log-collector/commit/a0dc1084ecac39043b8a5ea7c1681f985f9d73d6
    
    Change-Id: I1a49b23592fc7665948b632f28b57581d4332d56
    Bug-Url: https://bugzilla.redhat.com/show_bug.cgi?id=1539027
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/unpackAndPrepareDump.sh
+++ src/inventory_report/unpackAndPrepareDump.sh
@@ -81,11 +81,15 @@ fi
 
 tar -C "$(dirname $TAR_WITH_POSTGRES_SOSREPORT)" -Jxf "$TAR_WITH_POSTGRES_SOSREPORT"
 
-PG_DUMP_TAR=$(tar tf "$TAR_WITH_POSTGRES_SOSREPORT" | grep ".*pgdump.*tar.*") || :
+PG_DUMP_TAR=$(tar tf "$TAR_WITH_POSTGRES_SOSREPORT" | grep "pgdump-scl-rh-postgresql95.tar") || :
 if [ ! "${PG_DUMP_TAR}" ]; then
-    echo "Unable to detect the database dump from sosreport, aborting.." 1>&2
-    rm -rf "${UNPACKED_SOSREPORT} ${PG_DUMP_DIR}"
-    exit -1
+    # Compat mode - old postgresql dumps
+    PG_DUMP_TAR=$(tar tf "$TAR_WITH_POSTGRES_SOSREPORT" | grep "pgdump.tar") || :
+    if [ ! "${PG_DUMP_TAR}" ]; then
+        echo "Unable to detect the database dump from sosreport, aborting.." 1>&2
+        rm -rf "${UNPACKED_SOSREPORT} ${PG_DUMP_DIR}"
+        exit -1
+    fi
 fi
 
 echo "PG_DUMP_TAR=${UNPACKED_SOSREPORT}/*/log-collector-data/${PG_DUMP_TAR}" >> ${TMP_ROOT}/.metadata-inventory
