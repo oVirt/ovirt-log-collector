bc2401a9435a6fa46a3b99c234a571843682a1aa
Douglas Schilling Landgraf <dougsland@redhat.com>
2018-03-02 16:27:59 -0500

    inventory: increase timeout for large files
    sosreports can get pretty big and need a lot of time
    to upload, increasing the timeout.
    
    Change-Id: Ibd737bb02b9d7ae99537c2b5be5a6838dcea0726
    Signed-off-by: Douglas Schilling Landgraf <dougsland@redhat.com>

--- src/inventory_report/openshift/gunicorn_config.py
+++ src/inventory_report/openshift/gunicorn_config.py
@@ -17,6 +17,6 @@ bind = "127.0.0.1:8000"
 
 # Increasing the timeout because some sosreports can be big
 # and take up to 15m-20m (or more?) to process.
-timeout = "1800" # 1800 seconds is 30 minutes
+timeout = "3600" # 3600 seconds is 60 minutes
 #debug = True
 workers = multiprocessing.cpu_count() * 2 + 1
