oVirt/ovirt-log-collector

commit 7cbbff90872ac642486ac590f56cc9df99fe198b

ovirt-log-collector: added --batch option for unattended mode
Change-Id: I3425910216ded9679f490549ddabe93589de2235
Signed-off-by: Steffen Froemer <steffen@redhat.com>

src/__main__.py
@@ -1282,24 +1282,25 @@ class LogCollector(object):
 
         with_kerberos = bool(self.conf.get("kerberos"))
 
-        try:
-            self.conf.prompt("engine", msg="hostname of oVirt Engine")
-            if not with_kerberos:
-                self.conf.prompt(
-                    "user",
-                    msg="REST API username for oVirt Engine"
-                )
-                self.conf.getpass(
-                    "passwd",
-                    msg="REST API password for the %s oVirt Engine user" % (
-                        self.conf.get("user")
+        if not self.conf.get("quiet") and not self.conf.get("batch"):
+            try:
+                self.conf.prompt("engine", msg="hostname of oVirt Engine")
+                if not with_kerberos:
+                    self.conf.prompt(
+                        "user",
+                        msg="REST API username for oVirt Engine"
                     )
+                    self.conf.getpass(
+                        "passwd",
+                        msg="REST API password for the %s oVirt Engine user" % (
+                            self.conf.get("user")
+                        )
+                    )
+            except Configuration.SkipException:
+                logging.info(
+                    "Will not collect hypervisor list from oVirt Engine API."
                 )
-        except Configuration.SkipException:
-            logging.info(
-                "Will not collect hypervisor list from oVirt Engine API."
-            )
-            raise
+                raise
 
         try:
             return hypervisors.get_all(self.conf.get("engine"),
@@ -1506,7 +1507,7 @@ host=%(host_pattern)s):" % self.conf)
         hosts = self.conf.get("hosts")
 
         if hosts:
-            if not self.conf.get("quiet"):
+            if not self.conf.get("quiet") and not self.conf.get("batch"):
                 # Check if there are more than MAX_WARN_HOSTS_COUNT hosts
                 # to collect from
                 if len(hosts) >= MAX_WARN_HOSTS_COUNT:
@@ -1752,6 +1753,12 @@ to continue.
         "", "--quiet", dest="quiet",
         action="store_true", default=False,
         help="reduce console output (default=False)"
+    )
+
+    parser.add_option(
+        "", "--batch", dest="batch",
+        action="store_true", default=False,
+        help="batch mode - do not prompt interactively (default=False)"
     )
 
     parser.add_option(

commit f002e36e706df635f9292769e35fa6fb5571e15b

main: fix pep8 / flake8 issues
fixing some issues introduced with 7cbbff90872ac642486ac590f56cc9df99fe198b
which didn't go through CI.

Change-Id: I7641684a71ae8f33040474dfdf022fd96d14c12c
Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

src/__main__.py
@@ -1292,9 +1292,10 @@ class LogCollector(object):
                     )
                     self.conf.getpass(
                         "passwd",
-                        msg="REST API password for the %s oVirt Engine user" % (
-                            self.conf.get("user")
-                        )
+                        msg=(
+                            "REST API password for the %{user} "
+                            "oVirt Engine user"
+                        ).format(user=self.conf.get("user"))
                     )
             except Configuration.SkipException:
                 logging.info(
@@ -2092,7 +2093,7 @@ The directory is: %s'""" % (conf["local_scratch_dir"]))
                     hosts_present = collector.set_hosts(True)
                 elif not conf.get("no_hypervisor"):
                     hosts_present = collector.set_hosts()
-            except Exception as e:
+            except Exception:
                 pass
             collector.get_engine_data()
             collector.get_postgres_data()
@@ -2119,7 +2120,7 @@ The directory is: %s'""" % (conf["local_scratch_dir"]))
                     "No hypervisors were found, therefore no hypervisor \
 data will be listed.")
 
-    except KeyboardInterrupt as k:
+    except KeyboardInterrupt:
         print("Exiting on user cancel.")
     except Exception as e:
         multilog(logging.error, e)
@@ -2128,7 +2129,7 @@ data will be listed.")
         try:
             logging.debug("command: %s" % conf.command)
             # multilog(logging.debug, pprint.pformat(conf))
-        except:
+        except Exception:
             pass
         multilog(logging.debug, traceback.format_exc())
         sys.exit(ExitCodes.CRITICAL)