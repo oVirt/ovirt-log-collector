ff69a24bff6727a499575dfada70327ca79e3593
Sandro Bonazzola <sbonazzo@redhat.com>
2021-09-17 15:20:22 +0200

    automation: check RPM dependencies
    Change-Id: Ice3b8d83ad11cf6215df0742061dc617c544763f
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -4,3 +4,19 @@ autoreconf -ivf
 make distcheck
 
 ./automation/build-artifacts.sh
+
+echo -e "\n\n =====  Testing RPM Dependencies =====\n"
+# Restoring sane yum environment screwed up by mock-runner
+rm -f /etc/yum.conf
+dnf reinstall -y system-release dnf dnf-conf
+sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
+echo "deltarpm=False" >> /etc/dnf/dnf.conf
+rm -f /etc/yum/yum.conf
+
+dnf install -y https://resources.ovirt.org/pub/yum-repo/ovirt-release-master-tested.rpm
+if [[ "$(rpm --eval "%dist")" == ".el8" ]]; then
+dnf --downloadonly install ./exported-artifacts/*noarch.rpm
+else
+# we are still missing ovirt-engine on el9
+dnf --downloadonly install ./exported-artifacts/*noarch.rpm || true
+fi
